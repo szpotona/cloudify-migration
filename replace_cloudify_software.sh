93e34e34a2abc141660eca9a1e675fb6c2304a02
Szymon Bargłowski <szymon.barglowski@gmail.com>
2015-06-11 17:57:32 +0200

    Added scripts allowing for executing host_pre_stop and host_post_start operations in an "artificial workflow"

---
+++ replace_cloudify_software.sh
@@ -0,0 +1,11 @@
+
+if [[ $# -lt 3 ]]; then
+    echo "Usage: $0 blueprint_id deployment_id (migration_install|migration_uninstall)"
+    exit 1
+fi
+
+. /etc/default/celeryd-${2}_workflows
+
+execution_id=$(/opt/manager/bin/python create_execution.py $1 $2 $3)
+
+${VIRTUALENV}/bin/python software_replacement_workflow.py $1 $2 $execution_id $3
