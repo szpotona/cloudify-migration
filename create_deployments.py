c1ec690b9f469a864da8396eb45bc57e81abf0ff
Marcin Wasilewski <marcin.wasilewski@codilime.com>
2015-06-12 17:21:49 +0200

    First implementation of elasticsearch migration

--- create_deployments.py
+++ create_deployments.py
@@ -1,6 +1,8 @@
 import sys
 import json
+import os
 from cloudify_cli import utils
+from subprocess import call
 
 
 management_ip = utils.get_management_server_ip()
@@ -9,12 +11,27 @@ client = utils.get_rest_client(management_ip)
 deployments_json = sys.stdin.read()
 deployments = json.loads(deployments_json)
 
+host_magic_dir = "/tmp/cloudify_migration_data_2g25qt4/"
+
+del_template =  "curl -s -XDELETE 'http://localhost:9200/cloudify_storage/node_instance/_query?q=deployment_id:{} '"
+add_template = "curl -s -XPUT 'http://localhost:9200/cloudify_storage/node_instance/{id}/_create' -d \'{source}\'"
+
 if deployments:
     for dep in deployments:
-        new_dep = client.deployments.create(
-            dep['blueprint_id'],
-            dep['id'],
-            dep['inputs']
-        )
+        #new_dep = client.deployments.create(
+        #    dep['blueprint_id'],
+        #    dep['id'],
+        #    dep['inputs']
+        #)
+
+        nodes = json.loads(open(host_magic_dir + dep['id'], 'r').read())
+        
+        with open(os.devnull, "w") as fnull:
+            del_command = del_template.format(dep['id'])
+            call(["cfy", "ssh", "-c", del_command ], stdout=fnull, stderr=fnull)
+            for node in nodes["hits"]["hits"]:
+                add_command = add_template.format(id = str(node["_id"]), source=json.dumps(node["_source"]) )
+                call(["cfy", "ssh", "-c", add_command ], stdout=fnull, stderr=fnull)
+ 
         print 'Recreated deployment %s' % (new_dep['id'],)
 
--- retrieve_deployments.py
+++ retrieve_deployments.py
@@ -1,6 +1,7 @@
 from cloudify_cli import utils
 import json
 import os
+from subprocess import call
 
 
 management_ip = utils.get_management_server_ip()
@@ -11,3 +12,20 @@ deployments = client.deployments.list()
 result_f = os.fdopen(3, 'w')
 result_f.write(json.dumps(deployments))
 result_f.close()
+
+magic_file = "cloudify_migration_data_file_3f53t9"
+magic_path = "/tmp/" + magic_file
+host_magic_dir = "/tmp/cloudify_migration_data_2g25qt4/"
+
+dump_template = "curl -s -XGET 'http://localhost:9200/cloudify_storage/node_instance/_search?size=10000&q=deployment_id:{id}' > {tempfile}"
+
+if not os.path.exists(host_magic_dir):
+    os.makedir(host_magic_dir)
+
+with open(os.devnull, "w") as FNULL:
+    for dep in deployments:
+        dep_id = dep["id"]
+        call(["cfy", "ssh", "-c", dump_template.format(id=dep_id, tempfile=magic_path)], stdout=FNULL, stderr=FNULL)
+        scp(magic_path, host_magic_dir + dep_id)
+
+
