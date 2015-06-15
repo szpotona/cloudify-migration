cc9c3f91df592e6d6094e6031c8a5c6361207051
Marcin Wasilewski <marcin.wasilewski@codilime.com>
2015-06-15 12:35:22 +0200

    Migration of cloudify_events index from elasticsearch.

--- create_deployments.py
+++ create_deployments.py
@@ -15,7 +15,7 @@ deployments = json.loads(deployments_json)
 host_magic_dir = "/tmp/cloudify_migration_data_2g25qt4/"
 
 del_template =  "curl -s -XDELETE 'http://localhost:9200/cloudify_storage/node_instance/_query?q=deployment_id:{} '"
-add_template = "curl -s -XPUT 'http://localhost:9200/cloudify_storage/{type}/{id}/_create' -d \'{source}\'"
+add_template = "curl -s -XPUT 'http://localhost:9200/{index}/{type}/{id}/_create' -d \'{source}\'"
 
 if deployments:
     for dep in deployments:
@@ -25,7 +25,8 @@ if deployments:
             dep['inputs']
         )
 
-        nodes = json.loads(open(host_magic_dir + dep['id'], 'r').read())
+        nodes = json.loads(open(host_magic_dir + dep['id'] + "_storage", 'r').read())
+        events = json.loads(open(host_magic_dir + dep['id'] + "_events", 'r').read())
         
         with open(os.devnull, "w") as FNULL:
             del_command = del_template.format(dep['id'])
@@ -34,7 +35,10 @@ if deployments:
                 source = json.dumps(node["_source"])
                 if node["_type"] == "execution":
                     source = re.sub(r'/3\.1/',r'/3.2/', source) 
-                add_command = add_template.format(type=str(node["_type"]), id = str(node["_id"]), source=source)
+                add_command = add_template.format(index="cloudify_storage", type=str(node["_type"]), id=str(node["_id"]), source=source)
+                call(["cfy", "ssh", "-c", add_command ], stdout=FNULL, stderr=FNULL)
+            for event in events["hits"]["hits"]:
+                add_command = add_template.format(index="cloudify_events", type=str(event["_type"]), id=str(event["_id"]), source=json.dumps(event["_source"]))
                 call(["cfy", "ssh", "-c", add_command ], stdout=FNULL, stderr=FNULL)
 
  
--- retrieve_deployments.py
+++ retrieve_deployments.py
@@ -10,11 +10,12 @@ management_ip = utils.get_management_server_ip()
 client = utils.get_rest_client(management_ip)
 deployments = client.deployments.list()
 
-magic_file = "cloudify_migration_data_file_3f53t9"
-magic_path = "/tmp/" + magic_file
+magic_path = "/tmp/cloudify_migration_data_file_3f53t9"
+magic_path2 = "/tmp/cloudify_migration_data_file_25tg26f2"
 host_magic_dir = "/tmp/cloudify_migration_data_2g25qt4/"
 
-dump_template = "curl -s -XGET 'http://localhost:9200/cloudify_storage/node_instance,execution/_search?size=10000&q=deployment_id:{id}' > {tempfile}"
+dump_storage_template = "curl -s -XGET 'http://localhost:9200/cloudify_storage/node_instance,execution/_search?size=10000&q=deployment_id:{id}' > {tempfile}"
+dump_events_template = "curl -s -XGET 'http://localhost:9200/cloudify_events/_search?size=10000&q=deployment_id:{id}' > {tempfile}"
 
 if not os.path.exists(host_magic_dir):
     os.mkdir(host_magic_dir)
@@ -22,8 +23,10 @@ if not os.path.exists(host_magic_dir):
 with open(os.devnull, "w") as FNULL:
     for dep in deployments:
         dep_id = dep["id"]
-        call(["cfy", "ssh", "-c", dump_template.format(id=dep_id, tempfile=magic_path)], stdout=FNULL, stderr=FNULL)
-        scp(host_magic_dir + dep_id, magic_path, False)
+        call(["cfy", "ssh", "-c", dump_storage_template.format(id=dep_id, tempfile=magic_path)], stdout=FNULL, stderr=FNULL)
+        call(["cfy", "ssh", "-c", dump_events_template.format(id=dep_id, tempfile=magic_path2)], stdout=FNULL, stderr=FNULL)
+        scp(host_magic_dir + dep_id + "_storage", magic_path, False)
+        scp(host_magic_dir + dep_id + "_events", magic_path2, False)
  
 # These statements have to be executed as last.
 # Send the data to another script, running in a different virtenv
