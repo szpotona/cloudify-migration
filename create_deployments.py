f3484e3411c8b13eb3d6d21a77e2638088057636
Marcin Wasilewski <marcin.wasilewski@codilime.com>
2015-06-15 15:06:35 +0200

    Change cloudify_events migration to use bulk api.

--- create_deployments.py
+++ create_deployments.py
@@ -4,6 +4,7 @@ import os
 import re
 from cloudify_cli import utils
 from subprocess import call
+from scp import scp
 
 
 management_ip = utils.get_management_server_ip()
@@ -13,9 +14,13 @@ deployments_json = sys.stdin.read()
 deployments = json.loads(deployments_json)
 
 host_magic_dir = "/tmp/cloudify_migration_data_2g25qt4/"
+magic_file = "/tmp/cloudify_migration_data_5ovg6bht"
+host_magic_file = "/tmp/cloudify_migration_data_646mo325h"
 
 del_template =  "curl -s -XDELETE 'http://localhost:9200/cloudify_storage/node_instance/_query?q=deployment_id:{} '"
 add_template = "curl -s -XPUT 'http://localhost:9200/{index}/{type}/{id}/_create' -d \'{source}\'"
+bulk_template = "curl -s XPOST 'http://localhost:9200/cloudify_events/_bulk' --data-binary @{file}"
+bulk_entry_template = '{{ create: {{ "_id": "{id}", "_type": "{type}"  }} }}\n{source}\n'
 
 if deployments:
     for dep in deployments:
@@ -37,10 +42,13 @@ if deployments:
                     source = re.sub(r'/3\.1/',r'/3.2/', source) 
                 add_command = add_template.format(index="cloudify_storage", type=str(node["_type"]), id=str(node["_id"]), source=source)
                 call(["cfy", "ssh", "-c", add_command ], stdout=FNULL, stderr=FNULL)
-            for event in events["hits"]["hits"]:
-                add_command = add_template.format(index="cloudify_events", type=str(event["_type"]), id=str(event["_id"]), source=json.dumps(event["_source"]))
-                call(["cfy", "ssh", "-c", add_command ], stdout=FNULL, stderr=FNULL)
-
+            def remove_newlines(s):
+                return s.replace('\n', ' ').replace('\r', '')
+            bulk = "".join([bulk_entry_template.format(id=str(event["_id"]), type=str(event["_type"]), source=remove_newlines(json.dumps(event["_source"])) ) for event in events["hits"]["hits"]] )
+            with open(host_magic_file, "w") as f:
+                f.write(bulk)
+            scp(host_magic_file, magic_file, True)
+            call(["cfy", "ssh", "-c",  bulk_template.format(file=magic_file)], stdout=FNULL, stderr=FNULL)
  
         print 'Recreated deployment %s' % (new_dep['id'],)
 
