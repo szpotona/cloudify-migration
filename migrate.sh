eff7fdca29b7ed97418e7ec3a39101e1adaecf43
Szymon Bargłowski <szymon.barglowski@gmail.com>
2015-06-17 18:08:08 +0200

    Smarter blueprint lookup in the downloaded blueprint's dir

--- migrate.sh
+++ migrate.sh
@@ -101,7 +101,7 @@ function update_blueprint {
         if ! diff --old-group-format=$'\e[0;31m%<\e[0m' \
                   --new-group-format=$'\e[0;32m%>\e[0m' \
                   --unchanged-group-format='' $1 $changed_blueprint; then
-            read -p "Do you accept these modifications? [y/n] " user_resp
+            read -p "Do you accept these modifications for $1? [y/n] " user_resp
             if [[ $user_resp =~ $USER_YES_RESP_REGEXP ]]; then
                 mv $changed_blueprint $1
             else
@@ -115,14 +115,20 @@ function update_blueprint {
 function update_and_upload_all_blueprints {
     activate_new_cli
     for blueprint_dir in $BLUEPRINTS_DIR/*; do
-        for potential_bpnt in $(find $blueprint_dir -name "*.yaml"); do
-            read -p "Is $potential_bpnt a blueprint you want to migrate? [y/n] " user_resp
-            if [[ $user_resp =~ $USER_YES_RESP_REGEXP ]]; then
-                update_blueprint $potential_bpnt
-                cfy blueprints upload -p $potential_bpnt -b $(basename $blueprint_dir)
-                break # We assume there should be only one proper blueprint in $blueprint_dir directory
-            fi
-        done
+        potential_bpnts=( $blueprint_dir/*.yaml )
+        if [ ${#potential_bpnts[@]} -eq 1 ]; then # There is only one yaml file - our blueprint
+            blueprint=${potential_bpnts[0]}
+        else                                     # There are more files than can possibly be a blueprint
+            for potential_bpnt in "${potential_bpnts[@]}"; do
+                read -p "Is $potential_bpnt a blueprint you want to migrate? [y/n] " user_resp
+                if [[ $user_resp =~ $USER_YES_RESP_REGEXP ]]; then
+                    blueprint=$potential_bpnt
+                    break # We assume there should be only one proper blueprint in $blueprint_dir directory
+                fi
+            done
+        fi
+        update_blueprint $blueprint
+        cfy blueprints upload -p $blueprint -b $(basename $blueprint_dir)
     done
 }
 
