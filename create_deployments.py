5f167f2a59504c58f41f332f957d197c36e0e890
Marcin Wasilewski <marcin.wasilewski@codilime.com>
2015-06-17 10:53:13 +0200

    Added compliance of some files with pep8.

--- create_deployments.py
+++ create_deployments.py
@@ -17,10 +17,14 @@ host_magic_dir = "/tmp/cloudify_migration_data_2g25qt4/"
 magic_file = "/tmp/cloudify_migration_data_5ovg6bht"
 host_magic_file = "/tmp/cloudify_migration_data_646mo325h"
 
-del_template =  "curl -s -XDELETE 'http://localhost:9200/cloudify_storage/node_instance/_query?q=deployment_id:{} '"
-add_template = "curl -s -XPUT 'http://localhost:9200/{index}/{type}/{id}/_create' -d \'{source}\'"
-bulk_template = "curl -s XPOST 'http://localhost:9200/cloudify_events/_bulk' --data-binary @{file}"
-bulk_entry_template = '{{ create: {{ "_id": "{id}", "_type": "{type}"  }} }}\n{source}\n'
+del_template = ("curl -s -XDELETE 'http://localhost:9200/"
+                "cloudify_storage/node_instance/_query?q=deployment_id:{} '")
+add_template = ("curl -s -XPUT 'http://localhost:9200/"
+                "{index}/{type}/{id}/_create' -d \'{source}\'")
+bulk_template = ("curl -s XPOST 'http://localhost:9200/"
+                 "cloudify_events/_bulk' --data-binary @{file}")
+bulk_entry_template = ('{{ create: {{ "_id": "{id}",'
+                       '"_type": "{type}"  }} }}\n{source}\n')
 
 if deployments:
     for dep in deployments:
@@ -30,25 +34,45 @@ if deployments:
             dep['inputs']
         )
 
-        nodes = json.loads(open(host_magic_dir + dep['id'] + "_storage", 'r').read())
-        events = json.loads(open(host_magic_dir + dep['id'] + "_events", 'r').read())
-        
+        nodes = json.loads(open(
+            host_magic_dir + dep['id'] + "_storage",
+            'r'
+        ).read())
+
+        events = json.loads(open(
+            host_magic_dir + dep['id'] + "_events",
+            'r').read()
+        )
+
         with open(os.devnull, "w") as FNULL:
             del_command = del_template.format(dep['id'])
-            call(["cfy", "ssh", "-c", del_command ], stdout=FNULL, stderr=FNULL)
+            call(["cfy", "ssh", "-c", del_command],
+                 istdout=FNULL, stderr=FNULL)
             for node in nodes["hits"]["hits"]:
                 source = json.dumps(node["_source"])
                 if node["_type"] == "execution":
-                    source = re.sub(r'/3\.1/',r'/3.2/', source) 
-                add_command = add_template.format(index="cloudify_storage", type=str(node["_type"]), id=str(node["_id"]), source=source)
-                call(["cfy", "ssh", "-c", add_command ], stdout=FNULL, stderr=FNULL)
+                    source = re.sub(r'/3\.1/', r'/3.2/', source)
+                add_command = add_template.format(
+                        index="cloudify_storage",
+                        type=str(node["_type"]),
+                        id=str(node["_id"]),
+                        source=source)
+                call(["cfy", "ssh", "-c", add_command],
+                     stdout=FNULL, stderr=FNULL)
+
             def remove_newlines(s):
                 return s.replace('\n', ' ').replace('\r', '')
-            bulk = "".join([bulk_entry_template.format(id=str(event["_id"]), type=str(event["_type"]), source=remove_newlines(json.dumps(event["_source"])) ) for event in events["hits"]["hits"]] )
+
+            bulk = "".join([bulk_entry_template.format(
+                id=str(event["_id"]),
+                type=str(event["_type"]),
+                source=remove_newlines(json.dumps(event["_source"]))
+                ) for event in events["hits"]["hits"]])
+
             with open(host_magic_file, "w") as f:
                 f.write(bulk)
             scp(host_magic_file, magic_file, True)
-            call(["cfy", "ssh", "-c",  bulk_template.format(file=magic_file)], stdout=FNULL, stderr=FNULL)
- 
-        print 'Recreated deployment %s' % (new_dep['id'],)
+            call(["cfy", "ssh", "-c",  bulk_template.format(file=magic_file)],
+                 stdout=FNULL, stderr=FNULL)
 
+        print 'Recreated deployment %s' % (new_dep['id'],)
--- retrieve_deployments.py
+++ retrieve_deployments.py
@@ -14,8 +14,13 @@ magic_path = "/tmp/cloudify_migration_data_file_3f53t9"
 magic_path2 = "/tmp/cloudify_migration_data_file_25tg26f2"
 host_magic_dir = "/tmp/cloudify_migration_data_2g25qt4/"
 
-dump_storage_template = "curl -s -XGET 'http://localhost:9200/cloudify_storage/node_instance,execution/_search?size=10000&q=deployment_id:{id}' > {tempfile}"
-dump_events_template = "curl -s -XGET 'http://localhost:9200/cloudify_events/_search?size=10000&q=deployment_id:{id}' > {tempfile}"
+dump_storage_template = (
+    "curl -s -XGET 'http://localhost:9200/"
+    "cloudify_storage/node_instance,execution/"
+    "_search?size=10000&q=deployment_id:{id}' > {tempfile}")
+dump_events_template = (
+    "curl -s -XGET 'http://localhost:9200/"
+    "cloudify_events/_search?size=10000&q=deployment_id:{id}' > {tempfile}")
 
 if not os.path.exists(host_magic_dir):
     os.mkdir(host_magic_dir)
@@ -23,16 +28,17 @@ if not os.path.exists(host_magic_dir):
 with open(os.devnull, "w") as FNULL:
     for dep in deployments:
         dep_id = dep["id"]
-        call(["cfy", "ssh", "-c", dump_storage_template.format(id=dep_id, tempfile=magic_path)], stdout=FNULL, stderr=FNULL)
-        call(["cfy", "ssh", "-c", dump_events_template.format(id=dep_id, tempfile=magic_path2)], stdout=FNULL, stderr=FNULL)
+        call(["cfy", "ssh", "-c",
+             dump_storage_template.format(id=dep_id, tempfile=magic_path)],
+             stdout=FNULL, stderr=FNULL)
+        call(["cfy", "ssh", "-c",
+             dump_events_template.format(id=dep_id, tempfile=magic_path2)],
+             stdout=FNULL, stderr=FNULL)
         scp(host_magic_dir + dep_id + "_storage", magic_path, False)
         scp(host_magic_dir + dep_id + "_events", magic_path2, False)
- 
+
 # These statements have to be executed as last.
 # Send the data to another script, running in a different virtenv
 result_f = os.fdopen(3, 'w')
 result_f.write(json.dumps(deployments))
 result_f.close()
-
-
-
