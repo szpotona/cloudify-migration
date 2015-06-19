6c9fe8d286cabe0f1fce1cdcec1ab40da491a950
Konrad Paziewski <konrad.paziewski@gmail.com>
2015-06-19 15:49:06 +0200

    Common agents cleanup

--- common_agents/software_replacement_workflow.py
+++ common_agents/software_replacement_workflow.py
@@ -1,20 +1,22 @@
-from cloudify.plugins.workflows import _host_pre_stop, _host_post_start, _is_host_node
-from cloudify.decorators import workflow
 import sys
 import time
 
+from cloudify.plugins.workflows import _host_post_start,\
+    _host_pre_stop, _is_host_node
+from cloudify.decorators import workflow
+
 
 ctx = {
-  'blueprint_id': sys.argv[1],
-  'deployment_id': sys.argv[2],
-  'execution_id': sys.argv[3],
-  'workflow_id': sys.argv[4]
+    'blueprint_id': sys.argv[1],
+    'deployment_id': sys.argv[2],
+    'execution_id': sys.argv[3],
+    'workflow_id': sys.argv[4]
 }
+
 
 @workflow
 def replace_host_software(ctx, **kwargs):
     graph = ctx.graph_mode()
-
     for node in ctx.nodes:
         for instance in node.instances:
             if _is_host_node(instance):
@@ -23,11 +25,11 @@ def replace_host_software(ctx, **kwargs):
                 else:
                     def generate_tasks_fun(instance):
                         return _host_post_start(instance) + [
-                            instance.execute_operation('cloudify.interfaces.monitoring.start')
+                            instance.execute_operation(
+                                'cloudify.interfaces.monitoring.start')
                         ]
                 sequence = graph.sequence()
                 sequence.add(*generate_tasks_fun(instance))
-
     time.sleep(2)
     graph.execute()
 
--- runners/run_on_manager.sh
+++ runners/run_on_manager.sh
@@ -2,8 +2,9 @@
 set -eax
 
 
-sudo mkdir -p /tmp/script
-sudo cp $1 /tmp/script/script.tar.gz
-sudo tar -xf /tmp/script/script.tar.gz -C /tmp/script/
-sudo /bin/bash -c "cd /tmp/script; ./run.sh"
+mkdir -p /tmp/script
+cp $1 /tmp/script/script.tar.gz
+tar -xf /tmp/script/script.tar.gz -C /tmp/script/
+/bin/bash -c "cd /tmp/script; ./run.sh"
+rm -rf /tmp/script
 
