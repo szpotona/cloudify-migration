f44b22fef9bd9d92605b8acb4d64b3738e4574e5
Szymon Bargłowski <szymon.barglowski@gmail.com>
2015-06-24 16:25:55 +0200

    Prepared our drop-in workflow to be executed like regular workflows

--- common_agents/software_replacement_workflow.py
+++ common_agents/software_replacement_workflow.py
@@ -1,46 +1,32 @@
 import sys
 import time
 
-from cloudify.plugins.workflows import _host_post_start,\
-    _host_pre_stop, _is_host_node
+from cloudify.exceptions import NonRecoverableError
 from cloudify.decorators import workflow
-
-# Cloudify 3.1 uses 3.0.24 celery, where
-# Celery objects do not have 'set_default' method.
-# As a workaround, we use function from private module _state.
-# For next migrations it should be changed to use
-# public method 'set_default'.
-from celery._state import set_default_app
-from cloudify import celery
-
-set_default_app(celery.celery)
-
-
-ctx = {
-    'blueprint_id': sys.argv[1],
-    'deployment_id': sys.argv[2],
-    'execution_id': sys.argv[3],
-    'workflow_id': sys.argv[4]
-}
+from cloudify.plugins.workflows import (
+    _is_host_node,
+    _host_post_start,
+    _host_pre_stop
+)
 
 
 @workflow
-def replace_host_software(ctx, **kwargs):
+# op_name should be either install or uninstall
+def replace_host_software(ctx, op_name, **kwargs):
     graph = ctx.graph_mode()
     for node in ctx.nodes:
         for instance in node.instances:
             if _is_host_node(instance):
-                if sys.argv[4] == "migration_uninstall":
+                if op_name == "uninstall":
                     generate_tasks_fun = _host_pre_stop
-                else:
+                elif op_name == "install":
                     def generate_tasks_fun(instance):
                         return _host_post_start(instance) + [
                             instance.execute_operation(
                                 'cloudify.interfaces.monitoring.start')
                         ]
+                else:
+                    raise NonRecoverableError('Unrecognized operation %s' % op_name)
                 sequence = graph.sequence()
                 sequence.add(*generate_tasks_fun(instance))
-    time.sleep(2)
     graph.execute()
-
-replace_host_software(__cloudify_context=ctx)
