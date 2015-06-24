b20460c3b06a315a69eba383ce61ac89a744cef8
Konrad Paziewski <konrad.paziewski@gmail.com>
2015-06-24 18:30:58 +0200

    Fixed operation ordering in modify_agents. Added some communicates.

--- common_agents/modify_agents.sh
+++ common_agents/modify_agents.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 set -e
 if [[ $# -lt 4 ]]; then
@@ -5,6 +6,7 @@ if [[ $# -lt 4 ]]; then
     exit 1
 fi
 
+echo "Running operation $4 for deployment $2"
 source /etc/default/celeryd-${2}_workflows
 cp software_replacement_workflow.py ${VIRTUALENV}/lib/python2.7/site-packages
 cp ${CELERY_WORK_DIR}/celeryd-includes ${CELERY_WORK_DIR}/celeryd-includes.backup
@@ -13,11 +15,11 @@ echo "INCLUDES=$INCLUDES,software_replacement_workflow" > ${CELERY_WORK_DIR}/cel
 
 service celeryd-${WORKER_MODIFIER} restart
 
+$3/bin/python execute.py $1 $2 $4
+
 mv ${CELERY_WORK_DIR}/celeryd-includes.backup  ${CELERY_WORK_DIR}/celeryd-includes
 rm -f ${VIRTUALENV}/lib/python2.7/site-packages/software_replacement_workflow.py*
 
-$3/bin/python execute.py $1 $2 $4
-
 service celeryd-${WORKER_MODIFIER} restart
 
-echo 'done'
+echo "Updating agents for deployment $2 finished"
