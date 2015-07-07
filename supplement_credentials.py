84f3ea33511337a29fb333224ef19de38030512c
Szymon Bargłowski <szymon.barglowski@gmail.com>
2015-07-07 16:40:12 +0200

    Supplementing credentials improved

--- supplement_credentials.py
+++ supplement_credentials.py
@@ -7,13 +7,22 @@ from cloudify_cli.bootstrap import tasks as bstasks
 
 manager_version = sys.argv[1]
 
+
+def let_user_supplement_credentials(settings):
+    print 'The script is not able to perform "cfy ssh" and is not able to fix it automatically..'
+    manager_key = raw_input("Please provide a path to the %s manager's key: " % manager_version)
+    settings.set_management_key(manager_key)
+    manager_user = raw_input("Please provide the %s manager's user name: " % manager_version)
+    settings.set_management_user(manager_user)
+
+
 with utils.update_wd_settings() as settings:
-    if settings.get_management_key() is None and settings.get_management_user() is None:
+    if settings.get_management_key() is None or settings.get_management_user() is None:
         provider_context = settings.get_provider_context()
         bs.read_manager_deployment_dump_if_needed(
             provider_context.get('cloudify', {}).get('manager_deployment')
         )
-        env = bs.load_env('manager') # literal string here ..
+        env = bs.load_env('manager') # literal string here, no way to improve it now..
         storage = env.storage
         for instance in storage.get_node_instances():
             manager_key = instance.runtime_properties.get(bstasks.MANAGER_KEY_PATH_RUNTIME_PROPERTY)
@@ -23,11 +32,9 @@ with utils.update_wd_settings() as settings:
             if manager_user:
                 settings.set_management_user(manager_user)
             if manager_user or manager_key:
+                if not manager_user or not manager_key:
+                    let_user_supplement_credentials(settings)
                 break
     else:
-        print 'The script is not able to perform "cfy ssh" and is not able to fix it automatically..'
-        manager_key = raw_input("Please provide a path to the %s manager's key: " % manager_version)
-        settings.set_management_key(manager_key)
-        manager_user = raw_input("Please provide the %s manager's user name: " % manager_version)
-        settings.set_management_user(manager_user)
+        let_user_supplement_credentials(settings)
 
