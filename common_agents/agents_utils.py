29b549f2ecdc6e8e549b958800359f45facf5b1b
Konrad Paziewski <konrad.paziewski@codilime.com>
2015-07-08 17:43:41 +0000

    Added extracting task retries attempts from event

--- common_agents/agents_utils.py
+++ common_agents/agents_utils.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import time
 
@@ -6,6 +7,8 @@ import manager_rest.es_storage_manager as es
 from manager_rest import models
 from manager_rest.storage_manager import instance as storage_manager_instance
 
+
+_TASK_RETRIES_REGEXP = re.compile('.*\[attempt (?P<retry>\d+)(/\d+)?\]$')
 
 def es_connection_from_storage_manager(sm):
     try:
@@ -41,14 +44,15 @@ def events_generator(execution_id, sm):
     events_received = 0
     events_batch_size = 100
     events_total = 1  # dummy value, can be anything > 0
-    while status not in models.Execution.END_STATES:
+    finished = False
+    while not finished:
         # first, lets retrieve all events:
         while events_received < events_total:
             response = es_connection.search(
                 index='cloudify_events',
                 body=_create_events_query_body(
                     execution_id,
-                    last_event,
+                    events_received,
                     events_batch_size
                 )
             )
@@ -57,15 +61,58 @@ def events_generator(execution_id, sm):
                 yield e
             events_received += len(events)
             events_total = response['hits']['total']
-        time.sleep(5)
         status = sm.get_execution(execution_id).status
+        finished = status in models.Execution.END_STATES
+        if not finished:
+            time.sleep(5)
+
+def task_retries_from_msg(msg, default):
+    match = _TASK_RETRIES_REGEXP.match(msg)
+    if match:
+        return int(match.group('retry'))
+    return int(default)
+
+
+def event_task_retries(event, default):
+    ctx = event.get('context', {})
+    if 'task_current_retries' in ctx:
+        return ctx.get('task_current_retries')
+    msg = event.get('message', {}).get('text', '')
+    return task_retries_from_msg(msg, default)
+
+    
+def _task_retries_from_msg_test():
+    print 'Testing _task_retries_from_msg_test'
+    inputs = [
+        'random message [attempt 4/19]',
+        'random message with infinite attempts [attempt 5]',
+        'random message with infinite attempts [attempt 6][attempt 7]',
+        'random message with no attempts'
+    ]
+    default_value = 143
+    results = [4, 5, 7, default_value]
+    for question, answer in zip(inputs, results):
+        res = task_retries_from_msg(question, default_value)
+        if res != answer:
+            raise Exception(
+                'Wrong result for "{}": expected {}, received {}'.format(
+                    question,
+                    answer,
+                    res
+                )
+            )
+    print 'Test succeeded'
 
 
 def main(args):
-    sm = storage_manager_instance()
-    execution_id = args[0]
-    for e in events_generator(execution_id, sm):
-        print e
+    if len(args) > 1:
+        sm = storage_manager_instance()
+        execution_id = args[1]
+        retries = map(lambda e: event_task_retries(e, 0), events_generator(execution_id, sm))
+        print retries
+        pass
+    else:
+        _task_retries_from_msg_test() 
 
 
 if __name__ == '__main__':
