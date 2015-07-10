f45afb53aac2455f4ba710947329f57516c4cf22
Konrad Paziewski <konrad.paziewski@codilime.com>
2015-07-10 14:26:45 +0200

    Added trap cleanup to runners scripts

--- runners/run_on_docker.sh
+++ runners/run_on_docker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ea
 
+function cleanup {
+    rm -rf ~/script
+}
+trap cleanup EXIT
+
 mkdir -p ~/script
 tar -xf $1 -C ~/script
 sudo docker exec cfy /bin/bash -c "cd /tmp/home/script; ./run.sh"
-sudo rm -rf ~/script
--- runners/run_on_manager.sh
+++ runners/run_on_manager.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -ea
 
+function cleanup {
+    rm -rf /tmp/script
+}
+trap cleanup EXIT
 
 mkdir -p /tmp/script
 cp $1 /tmp/script/script.tar.gz
 tar -xf /tmp/script/script.tar.gz -C /tmp/script/
 /bin/bash -c "cd /tmp/script; ./run.sh"
-rm -rf /tmp/script
-
