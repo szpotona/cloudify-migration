f0ffdc33e104cb656c9153d219831b4a77327e7a
Marcin Wasilewski <marcin.wasilewski@codilime.com>
2015-07-31 13:23:23 +0200

    Bug fix - now data is migrated properly.

--- migrate_metrics.sh
+++ migrate_metrics.sh
@@ -8,7 +8,7 @@ BASE_DIR=$(dirname $(readlink -e $0))
 . $BASE_DIR/common.sh
 
 function usage {
-    echo "Usage: $SCRIPT_NAME old_cli_venv old_cli_dir new_cli_venv new_cli_dir"
+    echo "Usage: $SCRIPT_NAME old_cli_venv old_cli_dir new_cli_venv new_cli_dir docker/nodocker"
 }
 
 if [[ $# != 5 ]]; then
@@ -20,14 +20,14 @@ perform_setup $1 $2 $3 $4
 
 function export_metrics {
     activate_old_cli
-    if [[ '$5' == '3.1' ]]; then
+    if [[ '$5' != 'docker' ]]; then
         cfy ssh -c "sudo tar -czf $OLD_MAGIC_PATH  /opt/influxdb/shared/data"
     else
         cd $BASE_DIR/common_metrics/export
         tar -czf package.tar.gz run.sh
         cd -
         run_operation $BASE_DIR/common_metrics/export/package.tar.gz run_on_docker.sh
-        cfy ssh -c "sudo docker cp cfy:$OLD_MAGIC_PATH $OLD_MAGIC_PATH"
+        cfy ssh -c "sudo docker cp cfy:$OLD_MAGIC_PATH /tmp"
         rm -f $BASE_DIR/common_metrics/export/package.tar.gz
     fi
 
@@ -37,12 +37,11 @@ function export_metrics {
 
 function import_metrics {
     activate_new_cli
-    mv $HOST_MAGIC_PATH $BASE_DIR/common_metrics/$HOST_FILE
+    mv $HOST_MAGIC_PATH $BASE_DIR/common_metrics/import/$HOST_FILE
     cd $BASE_DIR/common_metrics/import
     tar -czf package.tar.gz  run.sh $HOST_FILE
     cd -
     run_operation $BASE_DIR/common_metrics/import/package.tar.gz run_on_docker.sh
-    rm -f $HOST_MAGIC_PATH
     rm -f $BASE_DIR/common_metrics/import/$HOST_FILE $BASE_DIR/common_metrics/import/package.tar.gz
 }
 
